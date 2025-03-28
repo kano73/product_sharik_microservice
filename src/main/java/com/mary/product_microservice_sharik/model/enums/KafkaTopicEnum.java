cfb0838a4fe9c67bca693715487940382e368c97
Pavel73 <pasha.nenahov@gmail.com>
2025-03-28 23:01:37 +0100

    moved staff

--- src/main/java/com/mary/product_microservice_sharik/model/enums/KafkaTopicEnum.java
+++ src/main/java/com/mary/product_microservice_sharik/model/enums/KafkaTopicEnum.java
@@ -1,6 +1,12 @@
 package com.mary.product_microservice_sharik.model.enums;
 
 public enum KafkaTopicEnum {
-    ADMIN_TOPIC,
-    PRODUCT_TOPIC
+// PRODUCT
+    PRODUCT_BY_FILTER_TOPIC,
+    PRODUCT_BY_ID_TOPIC,
+    PRODUCT_SET_STATUS_TOPIC,
+    PRODUCT_CREATE_TOPIC,
+
+//HISTORY + CART
+    HISTORY_CART_FILTER_TOPIC,
 }
--- src/main/java/com/mary/product_microservice_sharik/service/kafka/KafkaConsumerService.java
+++ src/main/java/com/mary/product_microservice_sharik/service/kafka/KafkaConsumerService.java
@@ -1,13 +1,39 @@
 package com.mary.product_microservice_sharik.service.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerService {
+    @KafkaListener(
+            topics = "#{T(com.example.KafkaTopicEnum).PRODUCT_BY_FILTER_TOPIC.name()}",
+            groupId = "product_group")
+    public void productsByFilter(ConsumerRecord<String,String> message) {
 
-    @KafkaListener(topics = "example-topic", groupId = "my-group-id")
-    public void consumeMessage(String message) {
+        System.out.println("Получено сообщение: " + message);
+    }
+
+    @KafkaListener(
+            topics = "#{T(com.example.KafkaTopicEnum).PRODUCT_BY_ID_TOPIC.name()}",
+            groupId = "product_group")
+    public void productById(ConsumerRecord<String,String> message) {
+
+        System.out.println("Получено сообщение: " + message);
+    }
+
+    @KafkaListener(
+            topics = "#{T(com.example.KafkaTopicEnum).PRODUCT_SET_STATUS_TOPIC.name()}",
+            groupId = "product_group")
+    public void setProductStatus(ConsumerRecord<String,String> message) {
+
+        System.out.println("Получено сообщение: " + message);
+    }
+
+    @KafkaListener(
+            topics = "#{T(com.example.KafkaTopicEnum).PRODUCT_CREATE_TOPIC.name()}",
+            groupId = "product_group")
+    public void createProduct(ConsumerRecord<String,String> message) {
 
         System.out.println("Получено сообщение: " + message);
     }
