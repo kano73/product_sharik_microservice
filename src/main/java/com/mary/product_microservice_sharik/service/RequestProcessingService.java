183a251ee74d571eb1f376b15199449ddce7aa4f
Pavel73 <pasha.nenahov@gmail.com>
2025-03-30 22:21:15 +0200

    sav1323

--- src/main/java/com/mary/product_microservice_sharik/service/RequestProcessingService.java
+++ src/main/java/com/mary/product_microservice_sharik/service/RequestProcessingService.java
@@ -24,84 +24,68 @@ public class RequestProcessingService {
     private final ObjectMapper objectMapper;
 
     public void createProduct(ConsumerRecord<String, String> message) throws JsonProcessingException {
+        AddProductDTO newProductDTO;
         try {
-            // Извлекаем данные из запроса
-            String newProductJson = message.value();
-            AddProductDTO newProductDTO = objectMapper.readValue(newProductJson, AddProductDTO.class);
+            newProductDTO = objectMapper.readValue(message.value(), AddProductDTO.class);
 
-            // Обрабатываем запрос
-            productService.create(newProductDTO);
-
-            // Сериализуем результат в JSON
-            String resultJson = objectMapper.writeValueAsString(true);
-            sendResponse(message, resultJson, false);
         } catch (Exception e) {
-            String resultJson = objectMapper.writeValueAsString("External error");
-            sendResponse(message, resultJson, true);
+            sendResponse(message, "exception while parsing data from request", true);
+            throw e;
         }
+        // Обрабатываем запрос
+        productService.create(newProductDTO);
+        sendResponse(message, true, false);
     }
 
     public void sendProductsByFilter(ConsumerRecord<String, String> message) throws JsonProcessingException {
+        ProductSearchFilterDTO filter;
         try {
-            // Извлекаем данные из запроса
-            String filterJson = message.value();
-
-            ProductSearchFilterDTO filter = objectMapper.readValue(filterJson, ProductSearchFilterDTO.class);
-
-            if (filter == null) {
-                filter = ProductSearchFilterDTO.defaultFilter();
-            }
-
-            // Обрабатываем запрос
-            List<Product> products = productService.findProductsByFilterOnPage(filter);
-
-            // Сериализуем результат в JSON
-            String resultJson = objectMapper.writeValueAsString(products);
-            sendResponse(message, resultJson, false);
+            filter = objectMapper.readValue(message.value(), ProductSearchFilterDTO.class);
+            filter = filter == null ? ProductSearchFilterDTO.defaultFilter(): filter;
         } catch (Exception e) {
-            String resultJson = objectMapper.writeValueAsString("External error");
-            sendResponse(message, resultJson, true);
+            sendResponse(message, "exception while parsing data from request", true);
+            throw e;
         }
+        // Обрабатываем запрос
+        List<Product> products = productService.findProductsByFilterOnPage(filter);
+
+        sendResponse(message, products, false);
     }
 
     public void sendProductById(ConsumerRecord<String, String> message) throws JsonProcessingException {
+        String id;
         try {
-            // Извлекаем данные из запроса
-            String idJson = message.value();
-            String id = objectMapper.readValue(idJson, String.class);
+            id = objectMapper.readValue(message.value(), String.class);
 
-            // Обрабатываем запрос
-            Product product = productService.findById(id);
-
-            // Сериализуем результат в JSON
-            String resultJson = objectMapper.writeValueAsString(product);
-
-            sendResponse(message, resultJson, false);
         } catch (Exception e) {
-            String resultJson = objectMapper.writeValueAsString("External error");
-            sendResponse(message, resultJson,true);
+            sendResponse(message, "exception while parsing data from request",true);
+            throw e;
         }
+
+        // Обрабатываем запрос
+        Product product = productService.findById(id);
+
+        sendResponse(message, product, false);
     }
 
     public void setProductStatus(ConsumerRecord<String, String> message) throws JsonProcessingException {
+        SetProductStatusDTO productStatusDTO;
         try {
-            // Извлекаем данные из запроса
-            String prodStatJson = message.value();
-            SetProductStatusDTO productStatusDTO = objectMapper.readValue(prodStatJson, SetProductStatusDTO.class);
-
-            // Обрабатываем запрос
-            productService.setProductStatus(productStatusDTO);
-
-            // Сериализуем результат в JSON
-            String resultJson = objectMapper.writeValueAsString(true);
-            sendResponse(message, resultJson, false);
+            productStatusDTO = objectMapper.readValue(message.value(), SetProductStatusDTO.class);
         } catch (Exception e) {
-            String resultJson = objectMapper.writeValueAsString("External error");
-            sendResponse(message, resultJson, true);
+            sendResponse(message, "exception while parsing data from request", true);
+            throw e;
         }
+
+        // Обрабатываем запрос
+        productService.setProductStatus(productStatusDTO);
+
+        sendResponse(message, true, false);
     }
 
-    private void sendResponse(ConsumerRecord<String, String> message, String resultJson, boolean isError){
+    private <D> void sendResponse(ConsumerRecord<String, String> message, D data, boolean isError) throws JsonProcessingException {
+        String resultJson = objectMapper.writeValueAsString(data);
+
         // Создаем ответное сообщение
         Message<String> reply;
         if(isError){
